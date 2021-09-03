0dd983209abf236e17942720b5447e7b966cec3d
Reed Krawiec <reedkrawiec@gmail.com>
2021-09-03 15:41:12 -0400

    ADded square listing

--- main.js
+++ main.js
@@ -5,12 +5,68 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 
-const commands = [{
-  name: 'board',
-  description: 'Generates a bingo board!'
-}]; 
+const fs = require("fs");
+
+const commands = [
+  {
+    name: 'board',
+    description: 'Generates a bingo board!'
+  },
+  {
+    name: "list-squares",
+    description: "Lists all the squares on the board"
+  },
+  {
+    name: "add-square",
+    description: "Adds a square to the board"
+  },
+  {
+    name: "remove-square",
+    description: "Removes a square from the board"
+  }
+];
 
 const rest = new REST({ version: '9' }).setToken(creds.token);
+
+const player_map = {
+
+};
+
+const get_squares = () => {
+  const raw = fs.readFileSync("./squares.txt", "utf8");
+  const lines = raw.split("\n");
+  return lines;
+}
+
+const generate_board = () => {
+  const lines = get_squares();
+  const already_used_map = {};
+  const collected_random_squares = [];
+  const final_board = [[], [], [], [], []];
+  for (let a = 0; a < 24; a++) {
+    let random_index;
+    let selected_square;
+    do {
+      random_index = Math.floor(Math.random() * lines.length);
+      selected_square = lines[random_index];
+    } while (already_used_map[random_index]);
+    already_used_map[random_index] = true;
+    collected_random_squares.push({
+      selected_square,
+      index:random_index
+    });
+  }
+  for (let a = 0; a < final_board.length; a++) {
+    for (let b = 0; b < 5; b++) {
+      if (a === 2 && b === 2) {
+        final_board[a].push("FREE");
+      } else {
+        final_board[a].push(collected_random_squares.shift());
+      }
+    }
+  }
+  return final_board;
+}
 
 (async () => {
   try {
@@ -28,17 +84,25 @@ const rest = new REST({ version: '9' }).setToken(creds.token);
 })();
 
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
+
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
-
-  if (interaction.commandName === 'board') {
-    await interaction.reply('Creating board!');
+  switch(interaction.commandName){
+    case "board":
+      console.log(generate_board());
+      await interaction.reply('Creating board!');
+      break;
+    case "list-squares":
+      const squares = get_squares();
+      for(let [index,square] of squares.entries()){
+        squares[index] = `${index}: ${square}`;
+      }
+      await interaction.reply(`LISTING SQUARES:\n${squares.join("\n")}`);
+      break;
   }
 });
-
-
 
 client.login(creds.token);
